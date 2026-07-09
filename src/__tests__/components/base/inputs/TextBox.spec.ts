71661992ecad1ffbfb602d8e4a408664a00ca4b7
Paweł Papierkowski <pawel.papierkowski@gmail.com>
2026-07-09 15:18:36 +0200

    test(): add tests for DateTimePicker and TextBox.

---
+++ src/__tests__/components/base/inputs/TextBox.spec.ts
@@ -0,0 +1,114 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+
+import TextBox from '@/components/base/inputs/TextBox.vue';
+
+//
+
+/** Convenience function to create component. */
+function createComponent(
+  modelValue: string | null,
+  ident: string,
+  autocomplete: string,
+  placeholder: string,
+  disabled: boolean,
+  invalid: boolean,
+) {
+  return mount(TextBox, {
+    props: {
+      modelValue,
+      ident,
+      autocomplete,
+      placeholder,
+      disabled,
+      invalid,
+    },
+  });
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+
+/** Tests of TextBox component. */
+describe('TextBox', () => {
+  it('has correct presentation for standard state', async () => {
+    // Ensures component shows correctly for standard state.
+
+    // Arrange&Act: Create textbox.
+    const textBox = createComponent('Text in TextBox', 'someTextBox', 'off', 'Type here', false, false);
+
+    // Assert: Input state is correct.
+    const input = textBox.find('input');
+    expect(input.classes()).toStrictEqual([]);
+    expect(input.element.value).toBe('Text in TextBox');
+    expect(input.attributes('type')).toBe('text');
+    expect(input.attributes('autocomplete')).toBe('off');
+    expect(input.attributes('placeholder')).toBe('Type here');
+    expect(input.attributes('disabled')).toBeUndefined();
+    expect(input.attributes('tabindex')).toBe('0');
+
+    // Assert: Textbox has correct data-testid attribute.
+    expect(textBox.attributes('data-testid')).toBe('textbox_someTextBox');
+  });
+
+  it('has correct presentation for disabled state', async () => {
+    // Ensures component shows correctly for disabled state.
+
+    // Arrange&Act: Create textboxx.
+    const textBox = createComponent('Text in TextBox', '', '', '', true, false);
+
+    // Assert: Input state is correct.
+    const input = textBox.find('input');
+    expect(input.classes()).toStrictEqual([]);
+    expect(input.element.value).toBe('Text in TextBox');
+    expect(input.attributes('type')).toBe('text');
+    expect(input.attributes('autocomplete')).toBe('');
+    expect(input.attributes('placeholder')).toBe('');
+    expect(input.attributes('disabled')).toBeDefined();
+    expect(input.attributes('tabindex')).toBe('-1'); // disabled component is skipped when finding next component to focus on
+
+    // Assert: Textbox has correct data-testid attribute.
+    expect(textBox.attributes('data-testid')).toBe('textbox_');
+  });
+
+  it('has correct presentation for invalid state', async () => {
+    // Ensures component shows correctly for invalid state.
+
+    // Arrange&Act: Create textboxx.
+    const textBox = createComponent(null, '', 'on', '', false, true);
+
+    // Assert: Input state is correct.
+    const input = textBox.find('input');
+    expect(input.classes()).toStrictEqual(['err']);
+    expect(input.element.value).toBe(''); // null value is treated as empty string here
+    expect(input.attributes('type')).toBe('text');
+    expect(input.attributes('autocomplete')).toBe('on');
+    expect(input.attributes('placeholder')).toBe('');
+    expect(input.attributes('disabled')).toBeUndefined();
+    expect(input.attributes('tabindex')).toBe('0');
+
+    // Assert: Textbox has correct data-testid attribute.
+    expect(textBox.attributes('data-testid')).toBe('textbox_');
+  });
+
+  //
+
+  it('typing changes model value', async () => {
+    // Ensures component behaves correctly when user types something in input.
+
+    // Arrange: Create textbox and set focus.
+    const textBox = createComponent(null, 'someTextBox', 'off', 'Type here', false, false);
+
+    // Act: Type something in <input>.
+    const input = textBox.find('input');
+    await input.setValue('A');
+
+    // Assert: Input state is correct.
+    expect(input.element.value).toBe('A');
+
+    // Assert: Emitted correct value.
+    const emitted = textBox.emitted('update:modelValue');
+    expect(emitted).toHaveLength(1);
+    const result = emitted?.at(-1)![0] as string;
+    expect(result).toBe('A');
+  });
+});
---
+++ src/__tests__/components/base/inputs/datetimepicker/DateTimePicker.spec.ts
@@ -0,0 +1,129 @@
+import { describe, it, expect, vi } from 'vitest';
+import { mount } from '@vue/test-utils';
+
+import i18n from '@/code/lang/i18n.ts';
+
+import DateTimePicker from '@/components/base/inputs/datetimepicker/DateTimePicker.vue';
+import DatePicker from '@/components/base/inputs/datetimepicker/DatePicker.vue';
+import TimePicker from '@/components/base/inputs/datetimepicker/TimePicker.vue';
+
+//
+
+/** Convenience function to create component. */
+function createComponent(
+  modelValue: Date | null,
+  ident: string,
+  mode: 'date' | 'time' | 'datetime',
+  allowNull: boolean,
+  disabled: boolean,
+  invalid: boolean,
+  showWeeks: boolean,
+  dateTimeMin?: Date | null,
+  dateTimeMax?: Date | null,
+) {
+  return mount(DateTimePicker, {
+    global: {
+      plugins: [i18n],
+    },
+    props: {
+      modelValue,
+      ident,
+      mode,
+      allowNull,
+      disabled,
+      invalid,
+      showWeeks,
+      dateTimeMin,
+      dateTimeMax,
+    },
+  });
+}
+
+// ////////////////////////////////////////////////////////////////////////////
+
+/** Tests of DateTimePicker component. */
+describe('DateTimePicker', () => {
+  it('mode datetime', () => {
+    // Ensure component shows DatePicker and TimePicker with correct params for 'datetime' mode.
+
+    // Arrange: Set up date/time.
+    vi.setSystemTime(new Date('2026-05-21T04:07:00Z'));
+    const minDate: Date = new Date('2027-01-04T00:00:00Z');
+    const maxDate: Date = new Date('2027-01-22T00:00:00Z');
+
+    // Act: Create the component.
+    const dateTimePicker = createComponent(null, 'someDateTime', 'datetime', false, true, false, false, minDate, maxDate);
+
+    // Assert: State of DatePicker is correct.
+    const datePicker = dateTimePicker.findComponent(DatePicker);
+    expect(datePicker.exists()).toBe(true);
+    expect(datePicker.props('modelValue')).toBe(null);
+    expect(datePicker.props('ident')).toBe('someDateTime');
+    expect(datePicker.props('allowNull')).toBe(false);
+    expect(datePicker.props('disabled')).toBe(true);
+    expect(datePicker.props('invalid')).toBe(false);
+    expect(datePicker.props('showWeeks')).toBe(false);
+    expect(datePicker.props('dateTimeMin')).toBe(minDate);
+    expect(datePicker.props('dateTimeMax')).toBe(maxDate);
+
+    // Assert: State of TimePicker is correct.
+    const timePicker = dateTimePicker.findComponent(TimePicker);
+    expect(timePicker.exists()).toBe(true);
+    expect(timePicker.props('modelValue')).toBe(null);
+    expect(timePicker.props('ident')).toBe('someDateTime');
+    expect(timePicker.props('allowNull')).toBe(false);
+    expect(timePicker.props('disabled')).toBe(true);
+    expect(timePicker.props('invalid')).toBe(false);
+  });
+
+  it('mode date', () => {
+    // Ensure component shows DatePicker and TimePicker with correct params for 'date' mode.
+
+    // Arrange: Set up date/time.
+    vi.setSystemTime(new Date('2026-05-21T04:07:00Z'));
+    const someDate: Date = new Date('2027-01-10T00:00:00Z');
+
+    // Act: Create the component.
+    const dateTimePicker = createComponent(someDate, 'someDate', 'date', false, false, false, true);
+
+    // Assert: State of DatePicker is correct.
+    const datePicker = dateTimePicker.findComponent(DatePicker);
+    expect(datePicker.exists()).toBe(true);
+    expect(datePicker.props('modelValue')).toBe(someDate);
+    expect(datePicker.props('ident')).toBe('someDate');
+    expect(datePicker.props('allowNull')).toBe(false);
+    expect(datePicker.props('disabled')).toBe(false);
+    expect(datePicker.props('invalid')).toBe(false);
+    expect(datePicker.props('showWeeks')).toBe(true);
+    expect(datePicker.props('dateTimeMin')).toBe(null);
+    expect(datePicker.props('dateTimeMax')).toBe(null);
+
+    // Assert: State of TimePicker is correct.
+    const timePicker = dateTimePicker.findComponent(TimePicker);
+    expect(timePicker.exists()).toBe(false);
+  });
+
+  it('mode time', () => {
+    // Ensure component shows DatePicker and TimePicker with correct params for 'time' mode.
+
+    // Arrange: Set up date/time.
+    vi.setSystemTime(new Date('2026-05-21T04:07:00Z'));
+    const someDate: Date = new Date('2027-01-10T00:00:00Z');
+
+    // Act: Create the component.
+    const dateTimePicker = createComponent(someDate, 'someTime', 'time', true, false, true, false);
+
+    // Assert: State of DatePicker is correct.
+    const datePicker = dateTimePicker.findComponent(DatePicker);
+    expect(datePicker.exists()).toBe(false);
+
+    // Assert: State of TimePicker is correct.
+    const timePicker = dateTimePicker.findComponent(TimePicker);
+    expect(timePicker.exists()).toBe(true);
+    expect(timePicker.props('modelValue')).toBe(someDate);
+    expect(timePicker.props('ident')).toBe('someTime');
+    expect(timePicker.props('allowNull')).toBe(true);
+    expect(timePicker.props('disabled')).toBe(false);
+    expect(timePicker.props('invalid')).toBe(true);
+  });
+});
